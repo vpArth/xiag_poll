a426568e52c9f15b19198f475482e4eb9623f265
Alexander Deider <arth.medintech@gmail.com>
2021-03-12 01:25:09 +0700

    Import php di container

---
+++ app/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+use DI\ContainerBuilder;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder;
+$containerBuilder->addDefinitions(__DIR__ . '/config.php');
+
+return $containerBuilder->build();
